2c419d2093cf1996f249c7e3bfd7849a8c16a1db
Ian Davis <iandavis0527@gmail.com>
2022-02-04 12:00:45 -0500

    First working version of the broken out digital deception server

--- digital_deception_emulator/backend/rspan/views.py
+++ digital_deception_emulator/backend/rspan/views.py
@@ -7,6 +7,7 @@ from digital_deception_emulator.backend import templating
 @cherrypy.expose
 class RSPANView:
     def GET(self, subjectId="missing-subject-id", qualtrics=False):
+        cherrypy.log("Inside RSPAN view")
         return templating.env.get_template("rspan.html.j2").render(
             subjectId=subjectId, qualtrics=str(qualtrics).lower()
         )
