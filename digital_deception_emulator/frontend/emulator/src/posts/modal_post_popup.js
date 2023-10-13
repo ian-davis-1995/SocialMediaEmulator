f71d0a33ad81ab0a2fa3c8adf7503726cf0447b8
Ian Davis <iandavis0527@gmail.com>
2023-10-13 10:09:44 -0400

    Final updates for AFRL Inspire demo

--- digital_deception_emulator/frontend/emulator/src/posts/modal_post_popup.js
+++ digital_deception_emulator/frontend/emulator/src/posts/modal_post_popup.js
@@ -12,22 +12,38 @@ function ModalPostLayout(props) {
     let profilePicture = "";
 
     if (props.post.profilePicture !== undefined) {
-        profilePicture = <div className={"profile-picture-container"} id={props.id + "-modal-profile-picture-container"}>
-            <img src={props.post.profilePicture}
-                className={"profile-picture"}
-                alt={"User Profile"}
-                id={props.id + "-modal-profile-picture"} />
-        </div>
+        profilePicture = (
+            <div
+                className={"profile-picture-container"}
+                id={props.id + "-modal-profile-picture-container"}
+            >
+                <img
+                    src={props.post.profilePicture}
+                    className={"profile-picture"}
+                    alt={"User Profile"}
+                    id={props.id + "-modal-profile-picture"}
+                />
+            </div>
+        );
     }
 
     return (
         <div id={props.id}>
-            <div className={"post-title-container modal-post-title"} id={props.id + "-modal-post-title"}>
+            <div
+                className={"post-title-container modal-post-title"}
+                id={props.id + "-modal-post-title"}
+            >
                 {profilePicture}
-                <div className={"post-author-container"} id={props.id + "-modal-post-author-container"}>
+                <div
+                    className={"post-author-container"}
+                    id={props.id + "-modal-post-author-container"}
+                >
                     {props.post.author}
                 </div>
-                <div className={"post-date-container"} id={props.id + "-modal-post-date-container"}>
+                <div
+                    className={"post-date-container"}
+                    id={props.id + "-modal-post-date-container"}
+                >
                     {props.post.postDate}
                 </div>
                 {/* <button className={"button close-dialog-button"}>
@@ -36,17 +52,28 @@ function ModalPostLayout(props) {
                     </span>
                 </button> */}
             </div>
-            <hr className={"post-section-separator"} id={props.id + "-modal-post-section-separator"} />
-            <div id={props.id + "-modal-post-body"} className={"modal-post-body"}>
-                <PostBody post={props.post}
+            <hr
+                className={"post-section-separator"}
+                id={props.id + "-modal-post-section-separator"}
+            />
+            <div
+                id={props.id + "-modal-post-body"}
+                className={"modal-post-body"}
+            >
+                <PostBody
+                    post={props.post}
                     onStorySegmentRead={props.onStorySegmentRead}
                     onStoryCompleted={props.onStoryCompleted}
                     showHelpInstructions={props.showHelpInstructions}
                     onNewsPostClicked={props.onNewsPostClicked}
                     modal={true}
-                    id={props.post.id} />
+                    id={props.post.id}
+                />
             </div>
-            <div id={props.id + "-modal-replies-body"} className={"modal-replies-body"}>
+            <div
+                id={props.id + "-modal-replies-body"}
+                className={"modal-replies-body"}
+            >
                 {repliesSection}
             </div>
         </div>
@@ -62,12 +89,20 @@ function RepliesSection(props) {
     }
 
     return (
-        <div className={"modal-replies-container"} id={props.id + "-replies-container"}>
-            <hr className={"post-section-separator"} id={props.id + "-section-separator"} />
-            <div className={"modal-replies-height-container"} id={props.id + "-replies-height-container"}>
+        <div
+            className={"modal-replies-container"}
+            id={props.id + "-replies-container"}
+        >
+            <hr
+                className={"post-section-separator"}
+                id={props.id + "-section-separator"}
+            />
+            <div
+                className={"modal-replies-height-container"}
+                id={props.id + "-replies-height-container"}
+            >
                 {items}
             </div>
-
         </div>
     );
 }
@@ -76,23 +111,43 @@ export function ReplyLayout(props) {
     let profilePictureContainer = "";
 
     if (props.reply.profilePicture !== undefined) {
-        profilePictureContainer = <div className={"modal-profile-picture-container"} id={props.reply.id + "-profile-picture-container"}>
-            <img src={props.reply.profilePicture} className={"modal-profile-picture"} alt={"User Profile"}
-                id={props.reply.id + "-profile-picture"} />
-        </div>;
+        profilePictureContainer = (
+            <div
+                className={"modal-profile-picture-container"}
+                id={props.reply.id + "-profile-picture-container"}
+            >
+                <img
+                    src={props.reply.profilePicture}
+                    className={"modal-profile-picture"}
+                    alt={"User Profile"}
+                    id={props.reply.id + "-profile-picture"}
+                />
+            </div>
+        );
     }
 
     return (
         <div className={"modal-reply-container"} id={props.reply.id}>
-            <div className={"modal-reply-flex-wrapper"} id={props.reply.id + "-flex-wrapper"}>
+            <div
+                className={"modal-reply-flex-wrapper"}
+                id={props.reply.id + "-flex-wrapper"}
+            >
                 {profilePictureContainer}
-                <div className={"modal-reply-body"} id={props.reply.id + "-body"}>
-                    <h5 className={"modal-reply-author"} id={props.reply.id + "-author"}>{props.reply.author}</h5>
+                <div
+                    className={"modal-reply-body"}
+                    id={props.reply.id + "-body"}
+                >
+                    <h5
+                        className={"modal-reply-author"}
+                        id={props.reply.id + "-author"}
+                    >
+                        {props.reply.author}
+                    </h5>
                     {props.reply.body}
                 </div>
             </div>
 
-            <div className={"modal-reply-bar"} id={props.reply.id + "-button-bar"}>
+            {/* <div className={"modal-reply-bar"} id={props.reply.id + "-button-bar"}>
                 <button type="button"
                     className={"button modal-reply-button" + (props.seenChosen ? " chosen" : "")}
                     onClick={() => props.onSeenClicked()}
@@ -112,7 +167,7 @@ export function ReplyLayout(props) {
                         id={props.reply.id + "-unique-text"}>Unique
                     </div>
                 </button>
-            </div>
+            </div> */}
         </div>
     );
 }
@@ -121,16 +176,27 @@ export default function ModalPostPopup(props) {
     return (
         <div
             id={"modal-post-container-" + props.id}
-            className={"modal-dialog" + (props.visible ? " modal-visible" : " modal-hidden")}
-            onClick={() => props.onBackgroundClicked()}>
-            <div className={"modal-post-container"}
-                onClick={(event) => props.onDialogClicked(event)}>
-                {props.post !== null ?
-                    <ModalPostLayout post={props.post}
+            className={
+                "modal-dialog" +
+                (props.visible ? " modal-visible" : " modal-hidden")
+            }
+            onClick={() => props.onBackgroundClicked()}
+        >
+            <div
+                className={"modal-post-container"}
+                onClick={(event) => props.onDialogClicked(event)}
+            >
+                {props.post !== null ? (
+                    <ModalPostLayout
+                        post={props.post}
                         id={props.id}
                         onCloseClicked={props.onCloseClicked}
                         onStorySegmentRead={props.onStorySegmentRead}
-                        onStoryCompleted={props.onStoryCompleted} /> : ""}
+                        onStoryCompleted={props.onStoryCompleted}
+                    />
+                ) : (
+                    ""
+                )}
             </div>
         </div>
     );
